0712942e63850a8be7093600feee1b1cd1c2a7c8
Sati <satenikkhachatryan55@gmail.com>
2021-01-27 11:58:33 +0400

    added camera and text

--- app/src/main/java/com/sati/plantnet/MainActivity.kt
+++ app/src/main/java/com/sati/plantnet/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sati.plantnet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickCamera (view: View) {
+        val intent = Intent((MediaStore.ACTION_IMAGE_CAPTURE))
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
