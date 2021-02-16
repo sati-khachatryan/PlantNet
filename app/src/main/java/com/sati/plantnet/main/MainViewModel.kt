78fbaf464f45438e11c3af9ef2b6836e55de05c9
Vahagn <vahagn.1984@mail.ru>
2021-02-16 15:35:24 +0400

    network

--- app/src/main/java/com/sati/plantnet/main/MainViewModel.kt
+++ app/src/main/java/com/sati/plantnet/main/MainViewModel.kt
@@ -1,2 +1,19 @@
 package com.sati.plantnet.main
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sati.plantnet.rest.service.IPhotoAPIService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainViewModel : ViewModel() {
+
+
+    fun uploadImages(images: List<String>) {
+        viewModelScope.launch(Dispatchers.IO) {
+
+        }
+    }
+}
